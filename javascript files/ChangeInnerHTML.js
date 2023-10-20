24e531799810d9c63b3c1c07eb7b12757e2ef5b5
AKCcoding <alraeatwork@gmail.com>
2023-10-20 14:41:02 +0800

    My first JS/TS fundamental program

---
+++ javascript files/ChangeInnerHTML.js
@@ -0,0 +1,6 @@
+// No need to implement <script></script> in external files
+// Change Text Function
+
+function changeName() {
+  document.getElementById("demo").innerHTML = "My First JavaScript";
+}
