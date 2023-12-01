32a17eeeb2eaf08382222b6123b8d694edcc4217
AKCcoding <alraeatwork@gmail.com>
2023-12-01 23:18:56 +0800

    Remove spaces

--- Projects/Weather App/function.js
+++ Projects/Weather App/function.js
@@ -1,19 +1,21 @@
 //If you don't have your api this won't work
 const apiKey = process.env.apiKey;
-const apiUrl =
-  "https://api.openweathermap.org/data/2.5/weather?units=metric&q=";
+const apiUrl = "https://api.openweathermap.org/data/2.5/weather?units=metric&q=";
 
 const searchBox = document.querySelector(".search input");
 const searchBtn = document.querySelector(".search button");
 const weatherIcon = document.querySelector(".weather-icon");
 
-async function checkWeather(city) {
+async function checkWeather(city) 
+{
   const response = await fetch(apiUrl + city + `&appid=${apiKey}`);
 
-  if (response.status == 404) {
+  if (response.status == 404)
+  {
     document.querySelector(".error").style.display = "block";
     document.querySelector(".weather").style.display = "none";
-  } else {
+  }
+  else {
     var data = await response.json();
     document.querySelector(".city").innerHTML = data.name;
     document.querySelector(".temp").innerHTML =
